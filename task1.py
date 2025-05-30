8f5f39cc0e7b2b719ce19fce6a1ae3f605590444
Serhii <158744795+BurglachOK@users.noreply.github.com>
2025-05-31 00:13:46 +0200

    WIP

---
+++ task1.py
@@ -0,0 +1,25 @@
+import sys
+from pathlib import Path
+import shutil
+
+
+def sorting(dir_sorting: Path, dir_sorted: Path):
+    for item in dir_sorting.iterdir():
+        if item.is_dir():
+            # print(f'[{item.name}]')
+            sorting(item, dir_sorted)
+            continue
+        suffix = item.name.split('.')[-1]
+        while not (dir_sorted / suffix).exists():
+            (dir_sorted / suffix).mkdir(parents=True, exist_ok=True)
+        shutil.copy(dir_sorting / item.name, dir_sorted / suffix / item.name)
+        # print(f'[{item.name}]')
+  
+
+try:
+    sorting(Path(sys.argv[1]), Path(sys.argv[2]))
+except:
+    sorting(Path(sys.argv[1]), Path(sys.argv[1]) / 'dist')
+
+        # if item.is_dir():
+        #     pass
